3c227a1eb89fc827c019a729da7a848191859298
JEFFERSON300 <jeffersonpedro300@gmail.com>
2020-06-12 18:07:39 -0300

    alteração pronto para trabalhar no pandas

--- Py_FII2.py
+++ Py_FII2.py
@@ -23,12 +23,6 @@ title_data = table.thead.find_all("th")
 # dados dos fundos
 table_data = table.tbody.find_all("td")
 
-# separando apenas texto titulo, retira as tags
-title = [0]*len(title_data)
-
-for x in range(len(title_data)):
-    title[x] = title_data[x].getText()     
-
 # separando apenas dados, retira as tags
 number_rows = int(len(table_data)/len(title_data))
 
@@ -42,9 +36,23 @@ for x in range(number_rows):
         i +=1
 
 #criando dataframe
+
+title = ['codigo', 'setor', 'preco_atual', 'liquidez_diaria', 'dividendo',
+           'dividendyield', 'DY(3M)_acumulado', 'DY(6M)_acumulado', 
+           'DY(12M)_acumulado', 'DY(3M)_media', 'DY(6M)_media', 
+           'DY(12M)_media', 'DY_ano', 'variacao_preco', 'rent_periodo', 
+           'rent_acumulada', 'patri_liq', 'VPA', 'P/VPA', 'DY_patri', 
+           'var_patri', 'rent_patri_periodo', 'rent_patri_acumu',
+           'vacancia_fisica', 'vancancia_financeira', 'quantidade_ativos']
+
 data = elements_data
 df = pd.DataFrame(data)
 df.columns = title
+
+for names in title:
+    for i in range(len(df['codigo'])):
+        if (type(df[names][i]) == str):
+            df[names][i] =df[names][i].replace('.','').replace('R$ ','').replace('%','').replace(',','.')
 
 #salvando dataframe
 df.to_csv('dados_FIIs_FundsExplorer.csv', index=False, encoding='utf-32')
